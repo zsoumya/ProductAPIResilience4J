a38d5f2212c54184dae2caa1c13fe52f8091484a
root <root@upshift-javatransformer-deployment-686f97d857-ltthc>
2021-07-26 16:28:16 +0000

    Implementing Caching using Spring Cache and Spring Data Redis libraries

--- src/main/java/com/cognizant/cde/fego/samples/ProductAPIResilience4J/ProductApiResilience4JApplication.java
+++ src/main/java/com/cognizant/cde/fego/samples/ProductAPIResilience4J/ProductApiResilience4JApplication.java
@@ -5,17 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching()
 public class ProductApiResilience4JApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductApiResilience4JApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiResilience4JApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplateBuilder().build();
-	}
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
 }
--- src/main/java/com/cognizant/cde/fego/samples/ProductAPIResilience4J/controller/ProductsController.java
+++ src/main/java/com/cognizant/cde/fego/samples/ProductAPIResilience4J/controller/ProductsController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import brave.Tracer;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.CacheEvict;
 
 @RestController
 @RequestMapping("/products")
@@ -21,21 +24,25 @@ public class ProductsController {
     @Autowired
     private ProductsService productsService;
 
+    // @Cacheable(value = “<cache-name>”, key = “<#id>”)
     @GetMapping
     public List<Product> getProducts() {
         return this.productsService.getProducts();
     }
 
+    // @CachePut(value = “<cache-name”>, key = “<“#id>)
     @PostMapping
     public void createProduct(@RequestBody Product product) {
         this.productsService.createProduct(product);
     }
 
+    // @CachePut(value = “<cache-name”>, key = “<“#id>)
     @PutMapping("/{id}")
     public void updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
         this.productsService.updateProduct(product);
     }
 
+    // @CacheEvict (value = “<cache-name”>, key = “<“#id>)
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable("id") long id) {
         this.productsService.deleteProduct(id);
